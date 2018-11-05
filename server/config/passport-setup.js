82a293c431fdf2486f5bce54cc4f0c35e450c775
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-11-04 19:16:26 -0800

    google configuration

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -1,4 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const google = require('./config.js');
 
-passport.use
+passport.use(
+    new GoogleStrategy({
+        // options for the Google strategy.
+        clientID: google.clientID;
+        clientSecret: google.clientSecret;
+    }), () => {
+        // passport callback function
+    }
+)
